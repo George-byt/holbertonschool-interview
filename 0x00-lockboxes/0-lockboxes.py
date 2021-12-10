4977198d626d44062407ffc2b43451844ef2cddb
George-byt <jmoralesvalencia750@gmail.com>
2021-12-09 21:25:23 -0500

    Add: 0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
+'''
+Lockbox task: You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes
+'''
 
 
+# asdfasdfasfasdfsd
 def canUnlockAll(boxes):
-    if not boxes:
-        return False
-    if len(boxes) == 0:
-        return False
-    keys = []
-    keys.append(0)
-    for key in keys:
-        new_keys = boxes[key]
-        for new_key in new_keys:
-            if new_key not in keys and new_key < len(boxes):
-                keys.append(new_key)
-    if len(keys) == len(boxes):
-        return True
-    return False
+    '''true if it can unlock al box from 0'''
+    keys = set(boxes[0])
+    tmp = {0}
+    counter = 0
+
+    # print("boxes: ", boxes)
+    while (counter != len(tmp)):
+        counter = len(tmp)
+        for key in keys:
+            try:
+                tmp.update(boxes[key])
+            except Exception:
+                continue
+        keys.update(tmp)
+
+    # print(keys)
+    for i in range(len(boxes)):
+        if i not in keys:
+            return False
+
+    return True
