536fd34334e52e9915cafb03a1965fcdefab0651
George-byt <jmoralesvalencia750@gmail.com>
2022-03-17 20:01:21 -0500

    N queens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,63 +1,64 @@
 #!/usr/bin/python3
-"""program that solves the N queens problem"""
-
-import sys
-
-
-def queens(n):
-    """program that solves the N queens problem"""
-    trail = []
-    sets = set()
-    for column in range(n):
-        trail.append([0, column])
-        sets.add(column)
-
-    road = []
-    while trail:
-        [row, column] = trail.pop(0)
-        while road and (row < road[0][0]):
-            road.pop(0)
-        if road and (row == road[0][0]):
-            road[0] = [row, column]
-        else:
-            road.insert(0, [row, column])
-
-        nextsrows = row + 1
-        death = set()
-        for (i, j) in road:
-            death.add(j)
-            distance = nextsrows - i
-            if j - distance >= 0:
-                death.add(j - distance)
-            if j + distance < n:
-                death.add(j + distance)
-
-        safe = sets.difference(death)
-        if not safe:
-            if nextsrows == n:
-                temp = road.copy()
-                temp.reverse()
-                print(temp, flush=True)
-            road.pop(0)
-        else:
-            safe = list(safe)
-            safe.reverse()
-            for position in safe:
-                trail.insert(0, [nextsrows, position])
-
-
+"""The N Queens Problem"""
 if __name__ == '__main__':
+
+    import sys
+
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
-
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        exit(1)
-
+        sys.exit(1)
     try:
-        n = int(sys.argv[1])
-    except Exception:
+        size = int(sys.argv[1])
+    except BaseException:
         print("N must be a number")
-        exit(1)
-    queens(n)
+        sys.exit(1)
+    if size < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    def startSolve():
+        data = [[0 for j in range(size)] for i in range(size)]
+        checkRecursive(data, 0)
+        return
+
+    def checkRecursive(data, c):
+        if (c == size):
+            solution(data)
+            return True
+        ret = False
+        for i in range(size):
+            if (checkPosition(data, i, c)):
+                data[i][c] = 1
+                ret = checkRecursive(data, c + 1) or ret
+                data[i][c] = 0
+        return ret
+
+    def checkPosition(data, r, c):
+        for i in range(c):
+            if (data[r][i]):
+                return False
+        i = r
+        j = c
+        while i >= 0 and j >= 0:
+            if(data[i][j]):
+                return False
+            i = i - 1
+            j = j - 1
+        i = r
+        j = c
+        while j >= 0 and i < size:
+            if(data[i][j]):
+                return False
+            i = i + 1
+            j = j - 1
+        return True
+
+    def solution(data):
+        solve = []
+        for i in range(size):
+            for j in range(size):
+                if(data[i][j] is 1):
+                    solve.append([i, j])
+        print(solve)
+        solve.clear()
+    startSolve()
